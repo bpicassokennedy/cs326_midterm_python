975d09bd050533ea4f3d94bd64900a90b9ce5c13
bpicassokennedy <b3llakennedy@gmail.com>
2025-01-09 19:22:32 +0000

    erm

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # Programmer: Bella Picasso-Kennedy 
 # Purpose: CS 326 Midterm - Palindrome Checker 
 
+def menu():
+    print("--- PALINDROME CHECKER! ---")
+    print("Palindrome Defintion: a word, phrase, or sequence that reads the same backward as forward")
+    print("----------------------------")
+    print("1. Check to see if a word is a palindrome")
+    print("2. Exit ")
+    choice = input("Enter your choice: ") # reads in as a string, needs to be converted to an int so the while loop condition executes correctly
+    choice = int(choice)
+    while choice != 1 and choice != 2:
+        choice = input("Invalid option! Try again: ")
+        choice = int(choice)
+    return choice
+
 def user_input():
     word = input("Enter a word to check if it's a palindrome: ")
     word = word.strip() # new string with leading and trailing spaces removed
@@ -15,12 +28,17 @@ def is_palindrome(s): # same logic used as the c++ program
     return True
 
 def main():
-    menu()
-    input = user_input()
-    palindrome = is_palindrome(input)
-    if palindrome:
-        print(input, "is a palindrome! :)")
-    else:
-        print(input, "is not a palindrome :(")
+    while True: # python syntatically does not support a do while loop! 
+        choice = menu()
+        if choice == 1: # user wants to enter a word, phrase, or sequence!
+            input = user_input()
+            palindrome = is_palindrome(input)
+            if palindrome:
+                print(input, "is a palindrome! :)\n")
+            else:
+                print(input, "is not a palindrome :(\n")
+        else:
+            print("Goodbye! :)")
+            break
 if __name__ == "__main__":
     main()
